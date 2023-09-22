a78105e8608a90455a917d31aaa120f9da010e69
soieu <102034740+soieu@users.noreply.github.com>
2023-09-22 09:36:25 +0900

    [unrated] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT      ugb.title TITLE, ugb.board_id BOARD_ID, ugr.reply_id REPLY_ID, 
+            ugr.writer_id WRITER_ID, ugr.contents CONTENTS, 
+            DATE_FORMAT(ugr.created_date, '%Y-%m-%d') CREATED_DATE
+FROM        used_goods_board ugb
+    JOIN    used_goods_reply ugr
+    ON      ugb.board_id = ugr.board_id
+WHERE       DATE_FORMAT(ugb.CREATED_DATE, '%Y-%m') = '2022-10'
+ORDER BY    ugr.created_date ASC, ugb.title ASC
