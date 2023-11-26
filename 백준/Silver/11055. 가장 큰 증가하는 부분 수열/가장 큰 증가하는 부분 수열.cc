1ddb2e40f3b8b62d95fb1431fe344adf40178937
soieu <102034740+soieu@users.noreply.github.com>
2023-11-26 12:42:38 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -23,7 +23,7 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j <= i; j++)
+        for (int j = 0; j < i; j++)
         {
             if (arr[i] > arr[j])
             {
