5c822cf7b5d2de691a773b812559dd69321c844e
soieu <102034740+soieu@users.noreply.github.com>
2023-11-27 20:33:30 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    int sum = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+
+    cout << sum;
+
+    return 0;
+}
