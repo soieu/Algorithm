60e014e708ed9846d44f911ea4a969aa3beaaa0a
soieu <102034740+soieu@users.noreply.github.com>
2023-12-22 22:12:43 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * n - 1 - (2 * i); j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
