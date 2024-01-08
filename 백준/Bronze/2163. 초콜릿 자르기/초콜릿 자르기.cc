fc48b1f83196eae307f909d2e7cbc7a9f3d9863f
soieu <102034740+soieu@users.noreply.github.com>
2024-01-08 23:38:40 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+    cout << (n - 1) + ((m - 1) * n);
+
+    return 0;
+}
