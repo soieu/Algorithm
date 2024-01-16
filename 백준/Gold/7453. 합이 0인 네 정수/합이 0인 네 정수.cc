c26853c298796af3199885e077e87a708e686653
soieu <102034740+soieu@users.noreply.github.com>
2024-01-16 19:13:13 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 4268 ms, Memory: 330088 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
@@ -1,31 +1,51 @@
-#include <bits/stdc++.h>
-#define ll long long
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-ll nums[4001][4], n, ans;
-vector <ll> sums1,sums2;
 
-int main(){
-    cin >> n;
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < 4; j++) 
-            cin >> nums[i][j];
+long long a[4001] = {0,};
+long long b[4001] = {0,};
+long long c[4001] = {0,};
+long long d[4001] = {0,};
+vector<long long> ab;
+vector<long long> cd;
 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            sums1.push_back(nums[i][0]+nums[j][1]);
-            sums2.push_back(nums[i][2]+nums[j][3]);
-        }
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+
+	cin>>n;
+	
+	for(int i = 0; i< n; i++)
+	{
+		cin>>a[i]>>b[i]>>c[i]>>d[i];
+	}
+
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			ab.push_back(a[i] + b[j]);
+			cd.push_back(c[i] + d[j]);
+		}
+	}
+	sort(ab.begin(), ab.end());
+	sort(cd.begin(), cd.end());
+
+	long long count = 0;
+
+	for(int i = 0; i < ab.size(); i++){
+        int idx = lower_bound(cd.begin(),cd.end(),-ab[i]) - cd.begin();
+        int endIdx = upper_bound(cd.begin(),cd.end(),-ab[i]) - cd.begin();
+        count += endIdx - idx;
     }
 
-    sort(sums1.begin(),sums1.end());
-    sort(sums2.begin(),sums2.end());
+    cout << count;
 
 
-    for(int i = 0; i < sums1.size(); i++){
-        int idx = lower_bound(sums2.begin(),sums2.end(),-sums1[i]) - sums2.begin();
-        int endIdx = upper_bound(sums2.begin(),sums2.end(),-sums1[i]) - sums2.begin();
-        ans += endIdx - idx;
-    }
 
-    cout << ans;
+	return 0;
 }
