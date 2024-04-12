bd950fc05734cbef2bf224bf28068eb06fb09b7a
soieu <102034740+soieu@users.noreply.github.com>
2024-04-12 20:56:45 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2384 ms, Memory: 256864 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] arr = new int[n];
+        int[] prefixSum = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+            prefixSum[i] = arr[i] + (i == 0 ? 0 : prefixSum[i-1]);
+
+        }
+
+        for(int i = 0; i < m; i++) {
+            int t1 = sc.nextInt();
+            int t2 = sc.nextInt();
+            System.out.println(prefixSum[t2-1] - (t1 == 1 ? 0 : prefixSum[t1-2]));
+        }
+
+
+    }
+}
