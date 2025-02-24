68d33ab9004efb8c1f0810b120e8c2770f4770fe
soieu <102034740+soieu@users.noreply.github.com>
2025-02-24 23:10:00 +0900

    [level 3] Title: 단속카메라, Time: 6.73 ms, Memory: 58.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,25 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
+        Arrays.sort(routes, (a, b) -> a[1] - b[1]);
+        int out = routes[0][1];
         int answer = 1;
-
-        // 첫 번째 원소 기준으로 오름차순 정렬
-        Arrays.sort(routes, (a, b) -> Integer.compare(a[0], b[0]));
         
-        // 첫 번째 경로의 끝점을 초기값으로 설정
-        int curEnd = routes[0][1];
-        
-        for (int i = 1; i < routes.length; i++) {
-            // 현재 경로의 시작점이 이전 끝점보다 큰 경우, 새로운 카메라 설치
-            if (routes[i][0] > curEnd) {
+        for(int i = 1; i < routes.length; i++) {
+            if(routes[i][0] > out) {
+                out  = routes[i][1];
                 answer++;
-                curEnd = routes[i][1]; // 끝점 갱신
-            } else {
-                // 겹치는 구간이 있으면 curEnd를 더 작은 값으로 업데이트
-                curEnd = Math.min(curEnd, routes[i][1]);
-            }
+            } 
         }
-        
         return answer;
     }
 }
